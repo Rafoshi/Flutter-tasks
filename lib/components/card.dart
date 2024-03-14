dd15b7dc6469f757c399e7198f2154b23044b9a9
Rafaioshi <rafael.pereira@brgaap.com>
2024-03-14 00:06:08 -0300

    First commit

---
+++ lib/components/card.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+int nivel = 0;
+
+class TaskCard extends StatefulWidget {
+  final String nome;
+  const TaskCard(this.nome, {super.key});
+
+  @override
+  State<TaskCard> createState() => _TaskCardState();
+}
+
+class _TaskCardState extends State<TaskCard> {
+  int nivel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        DownPartCard(nivel),
+        UpperPartCard(widget.nome),
+      ],
+    );
+  }
+}
+
+class DownPartCard extends StatelessWidget {
+  final int nivel;
+  const DownPartCard(this.nivel, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 25,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      color: const Color.fromRGBO(47, 128, 237, 1),
+      child: SizedBox(
+        height: 130,
+        width: 1000,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const SizedBox(
+                    width: 160,
+                    child: LinearProgressIndicator(
+                      color: Colors.white,
+                      value: 0.8,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Nível: $nivel',
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UpperPartCard extends StatelessWidget {
+  final String nome;
+  const UpperPartCard(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 25,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      color: const Color.fromRGBO(255, 255, 255, 1),
+      child: SizedBox(
+        height: 90,
+        width: 1000,
+        child: Row(
+          children: [
+            const SizedBox(
+              width: 12,
+            ),
+            const Image(
+              width: 60,
+              image: NetworkImage(
+                  'https://static-00.iconduck.com/assets.00/todo-icon-2048x2048-m7wp6prw.png'),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 120,
+                  child: Text(
+                    nome,
+                    style: TextStyle(
+                        fontSize: 20, overflow: TextOverflow.ellipsis),
+                  ),
+                ),
+                Text('Aprender Flutter'),
+              ],
+            ),
+            const SizedBox(
+              width: 25,
+            ),
+            SizedBox(
+              height: 70,
+              width: 70,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(47, 128, 237, 1),
+                  padding: const EdgeInsets.all(0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+                onPressed: () {
+                  nivel++;
+                },
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.arrow_drop_up_outlined,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Lvl Up',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tasks/components/card.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromRGBO(208, 221, 237, 1),
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(47, 128, 237, 1),
+          title: const Text(
+            'Tarefas',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+          ),
+        ),
+        body: Column(
+          children: [
+            TaskCard('Meditar'),
+            TaskCard('Jogar'),
+          ],
+        ),
+      ),
+    );
+  }
+}
